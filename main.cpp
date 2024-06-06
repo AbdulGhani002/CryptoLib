253c9838a2db7aaca927e3df0a6072df00d04ca5
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2024-06-06 08:05:33 +0500

    Encryption algorithm is completed

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "CryptoLib.h"
-#include <iostream>
-#include <string>
-
-int main()
-{
-    std::cout << CryptoLib::encryptAES("Abdul Ghani", CryptoLib::generatePrimeNumber()) << std::endl;
-    return 0;
-}
